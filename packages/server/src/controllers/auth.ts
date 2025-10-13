90d88b3fb84209197a8575f8d798ee00fe5f6c42
Joseph Heidari <agile.8272@gmail.com>
2025-10-13 13:33:54 -0400

    Makes /auth/status route more robust
    Now it can handle expired and invalid sessions.

--- packages/server/src/controllers/auth.ts
+++ packages/server/src/controllers/auth.ts
@@ -33,6 +33,17 @@ const logOut = (req: Request, res: Response) => {
 
 // If user is logged in, return their info (email, username) else return false
 const checkAuthStatus = async (req: Request, res: Response) => {
+  // The function will generate a new session token, save it to the database, and send it as a cookie to the client
+  const generateSessionToken = async () => {
+    const sessionToken = crypto.randomBytes(16).toString("hex"); // 128 bits
+
+    await DB.query(`INSERT INTO sessions (session_token) VALUES ($1)`, [
+      sessionToken,
+    ]);
+
+    req.session.session_token = sessionToken; // automatically is signed and set as cookie
+  };
+
   if (req.user) {
     const user = await DB.find<IUser>(
       `SELECT email, username FROM users WHERE id=${req.user.id}`
@@ -44,24 +55,37 @@ const checkAuthStatus = async (req: Request, res: Response) => {
         email: user.email,
         username: user.username,
       });
+    } else {
+      // Something went wrong, log the user out
+      req.logout();
+      return res.json({ isSignedIn: false });
     }
   } else {
     let sessionToken = req.session?.session_token;
 
+    // User doesn't have a session token
     if (!sessionToken) {
-      sessionToken = crypto.randomBytes(16).toString("hex"); // 128 bits
+      await generateSessionToken();
+    } else {
+      /* User do have a session token, but we don't know yet if it's valid or expired */
 
-      await DB.query(`INSERT INTO sessions (session_token) VALUES ($1)`, [
-        sessionToken,
-      ]);
+      // Check if the supplied session token exists and not expired
+      const session = await DB.find<ISession>(
+        `SELECT id FROM sessions WHERE session_token = $1 AND expires_at > NOW()`,
+        [sessionToken]
+      );
 
-      req.session.session_token = sessionToken; // automatically is signed and set as cookie
+      if (session) {
+        // If session exists and it valid, update last_active
+        await DB.query(
+          `UPDATE sessions SET last_active = NOW() WHERE session_token = $1`,
+          [sessionToken]
+        );
+      } else {
+        // If session doesn't exist, create a new one
+        await generateSessionToken();
+      }
     }
-
-    await DB.query(
-      `UPDATE sessions SET last_active = NOW() WHERE session_token = $1`,
-      [sessionToken]
-    );
 
     res.json({ isSignedIn: false });
   }
